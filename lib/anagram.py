0cad841b266b466916905f8b2662249af091954b
Your Name <youremail@domain.com>
2024-12-01 01:19:19 +0300

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word.lower()) == sorted_word]
+
